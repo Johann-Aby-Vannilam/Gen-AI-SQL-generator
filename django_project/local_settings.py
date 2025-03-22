2679933688823fe24c387e330cc3c7321e7b6ad2
neonithinar <neonithinar@gmail.com>
2025-03-22 14:53:28 +0530

    created local settings file, updated environment variables and gitignore

---
+++ django_project/local_settings.py
@@ -0,0 +1,11 @@
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sql-generator',  # Replace with your database name
+        'USER': 'postgres',   # Replace with your PostgreSQL username
+        'PASSWORD': 'Nithin@1992',  
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
